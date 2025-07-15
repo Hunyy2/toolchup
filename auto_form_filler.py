4f15be344550ef37f6cf948f1cb7a141b365e94f
Ngo_Nhat_Huy <ngonhathuy501@gmail.com>
2025-07-15 22:00:06 +0700

    lại fix

--- auto_form_filler.py
+++ auto_form_filler.py
@@ -808,7 +808,7 @@ if __name__ == "__main__":
     multiprocessing.freeze_support()
 
     # !!! THAY ĐỔI URL NÀY KHI BẠN DEPLOY LÊN RENDER !!!
-    BACKEND_URL = "https://your-backend-app-name.onrender.com"
+    BACKEND_URL = "https://toolchup.onrender.com"
     # BACKEND_URL = "http://127.0.0.1:5000"
     # Để test local, dùng: BACKEND_URL = "http://127.0.0.1:5000"
 
